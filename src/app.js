f0da3f458850d9dffb3400cf1f887c897f0512a3
Nil <nildiazb@gmail.com>
2025-05-14 08:20:01 +0200

    Eliminacio edit.ejs i canvis a app.js

--- src/app.js
+++ src/app.js
@@ -11,33 +11,6 @@ const Actuacio = require('./models/Actuacio');
 const Tipu = require('./models/Tipu');
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://a18hugcorcob:<Holapedralbes_01>@cluster0.yoo2cak.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
 // Relacions
 Departament.hasMany(Incidencia, { foreignKey: 'id_departament', onDelete: 'SET NULL', onUpdate: 'CASCADE' });
 Incidencia.belongsTo(Departament, { foreignKey: 'id_departament', onUpdate: 'CASCADE' });
